c108c04cf28307eb67499af8b26bef8d6ef18ca2
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-06-10 12:25:06 +0300

    build: remove build target and Vite build configuration

--- packages/zod-to-model-definitions/vite.config.ts
+++ packages/zod-to-model-definitions/vite.config.ts
@@ -2,19 +2,11 @@
 import { defineConfig } from 'vite';
 
 import viteTsConfigPaths from 'vite-tsconfig-paths';
-import dts from 'vite-plugin-dts';
-import { joinPathFragments } from '@nx/devkit';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/zod-to-model-definitions',
 
   plugins: [
-    dts({
-      entryRoot: 'src',
-      tsConfigFilePath: joinPathFragments(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
-    }),
-
     viteTsConfigPaths({
       root: '../../',
     }),
@@ -28,24 +20,6 @@ export default defineConfig({
   //    }),
   //  ],
   // },
-
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'zod-to-model-definitions',
-      fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forgot to update your package.json as well.
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: [],
-    },
-  },
 
   test: {
     globals: true,
