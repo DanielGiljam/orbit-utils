71c368a001d1a5fcda99aefc67312b70e320390e
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2023-06-10 14:44:11 +0300

    build: add basic metadata

--- packages/zod-to-model-definition/tsup.config.ts
+++ packages/zod-to-model-definition/tsup.config.ts
@@ -1,5 +1,10 @@
 import {defineConfig} from "tsup";
 import {exec} from "child_process";
+import * as devkit from "@nx/devkit";
+
+const {readJsonFile, writeJsonFile} = (
+    devkit as unknown as {default: typeof devkit}
+).default;
 
 const execAsync = async (command: string): Promise<void> =>
     await new Promise((resolve, reject) => {
@@ -14,7 +19,8 @@ const execAsync = async (command: string): Promise<void> =>
         childProcess.stderr!.pipe(process.stderr);
     });
 
-const outDir = "../../dist/packages/zod-to-model-definition";
+const projectName = "zod-to-model-definition";
+const outDir = `../../dist/packages/${projectName}`;
 
 export default defineConfig({
     entry: ["src/index.ts"],
@@ -28,8 +34,24 @@ export default defineConfig({
             `pnpm tsc --declaration --emitDeclarationOnly --project tsconfig.lib.json --outFile ${outDir}/index.d.ts`,
         );
         console.log("  - Emitted TypeScript declaration files.");
-        await execAsync(`cp package.json README.md ${outDir}`);
-        console.log("  - Copied package.json and README.md.");
+        await execAsync(`cp README.md ../../LICENSE ${outDir}`);
+        console.log("  - Copied README.md and ../../LICENSE to out directory.");
+        const pkg = await readJsonFile("package.json");
+        const rootPkg = await readJsonFile("../../package.json");
+        pkg.homepage = rootPkg.homepage.replace(
+            /#readme$/,
+            `/tree/main/packages/${projectName}#readme`,
+        );
+        pkg.bugs = rootPkg.bugs.concat(`+label%3A${projectName}`);
+        pkg.license = rootPkg.license;
+        pkg.author = rootPkg.author;
+        pkg.repository = {...rootPkg.repository};
+        pkg.repository.directory = `packages/${projectName}`;
+        await writeJsonFile(`${outDir}/package.json`, pkg);
+        console.log(
+            "  - Merged package.json and ../../package.json and wrote to out directory.",
+        );
+        console.log("  - Done.");
     },
     outDir,
 });
